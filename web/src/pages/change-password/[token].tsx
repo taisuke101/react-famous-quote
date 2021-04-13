aab06a723d73f625cff4750994e054a060a899bf
TaisukeYamamoto <uwattotaitai@gmail.com>
2021-04-13 14:18:11 +0900

    パスワード変更後処理の追加

--- web/src/pages/change-password/[token].tsx
+++ web/src/pages/change-password/[token].tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { useState, VFC } from 'react';
 import { useForm } from 'react-hook-form';
@@ -15,13 +16,15 @@ const Token: VFC<TokenProps> = ({}) => {
 
     const token = router.query.token;
 
+    const [complete, setComplete] = useState(false);
+
     const [ error, setError ] = useState<Record<string, string>>({});
 
     const { register, handleSubmit } = useForm<TokenProps>();
 
     const [changePassword, { loading }] = useChangePasswordMutation({
         onError: (err) => setError(err.graphQLErrors[0].extensions?.formattedErrors),
-        onCompleted: () => router.push('/'),
+        onCompleted: () => setComplete(true),
     });
 
     const onSubmit = (value: ChangePasswordInput) => {
@@ -33,36 +36,46 @@ const Token: VFC<TokenProps> = ({}) => {
     
     return (
         <div className='pt-24'>
-            {
-                loading ? (
-                    <div className='flex justify-center'>
-                        <h1>Loading....</h1>
-                        <Loader
-                            type="TailSpin"
-                            color="#00fa9a"
-                            height={200}
-                            width={200}
-                        />
-                    </div>
-                ) : (
-                    <form
-                        onSubmit={handleSubmit(onSubmit)}
-                        className='flex flex-col mx-40 text-center'
-                    >
-                        <p className='mt-2 text-xl'>新しいパスワード</p>
-                        { error.newPassword && <p className='text-red-600'>{error.newPassword}</p> }
-                        <input 
-                            name='newPassword'
-                            placeholder='新しいパスワード'
-                            className='p-2 mt-2 border-b-2 border-green-400'
-                            {...register('newPassword')}
-                        />
-                        <SubmitButton>
-                            登録
-                        </SubmitButton>
-                    </form>
-                )
-            }
+            <>
+                {
+                    loading ? (
+                        <div className='flex justify-center'>
+                            <h1>Loading....</h1>
+                            <Loader
+                                type="TailSpin"
+                                color="#00fa9a"
+                                height={200}
+                                width={200}
+                            />
+                        </div>
+                    ) : (
+                        complete ? (
+                            <>
+                                <p>パスワードの再設定が完了しました！</p>
+                                <Link href='/login'><a>ログインする</a></Link>
+                                <Link href='/'><a>ホームに戻る</a></Link>
+                            </>
+                        ) : (
+                            <form
+                                onSubmit={handleSubmit(onSubmit)}
+                                className='flex flex-col mx-40 text-center'
+                            >
+                                <p className='mt-2 text-xl'>新しいパスワード</p>
+                                { error.newPassword && <p className='text-red-600'>{error.newPassword}</p> }
+                                <input 
+                                    name='newPassword'
+                                    placeholder='新しいパスワード'
+                                    className='p-2 mt-2 border-b-2 border-green-400'
+                                    {...register('newPassword')}
+                                />
+                                <SubmitButton>
+                                    登録
+                                </SubmitButton>
+                            </form>
+                        )
+                    )
+                }
+            </>
         </div>
     );
 }
